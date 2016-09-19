f903cc59ff15d8afb858e6f091a5470e0fad406c
kschrenk <kornel.schrenk@gmail.com>
2016-09-19 18:17:09 +0200

    Moved from old SD directory to SdFat.

--- BleSdRemote.ino
+++ BleSdRemote.ino
@@ -1,6 +1,6 @@
-#include <Arduino.h>
-#include <SPI.h>
-#include <SD.h>
+#include "Arduino.h"
+#include "SPI.h"
+#include "SdFat.h"
 
 #include "Adafruit_BLE.h"
 #include "Adafruit_BluefruitLE_SPI.h"
@@ -12,10 +12,10 @@
 #define FACTORYRESET_ENABLE 0
 
 #if defined(HAVE_HWSERIAL1) //Use the first physical serial UART on Arduino Mega or Due
-	Adafruit_BluefruitLE_UART ble(Serial1, BLUEFRUIT_UART_MODE_PIN);
+Adafruit_BluefruitLE_UART ble(Serial1, BLUEFRUIT_UART_MODE_PIN);
 #else //Use Software Serial interface -- Do not forget to adjust the configuration!!!
-	SoftwareSerial bluefruitSS = SoftwareSerial(BLUEFRUIT_SWUART_TXD_PIN, BLUEFRUIT_SWUART_RXD_PIN);
-	Adafruit_BluefruitLE_UART ble(bluefruitSS, BLUEFRUIT_UART_MODE_PIN, BLUEFRUIT_UART_CTS_PIN, BLUEFRUIT_UART_RTS_PIN);
+SoftwareSerial bluefruitSS = SoftwareSerial(BLUEFRUIT_SWUART_TXD_PIN, BLUEFRUIT_SWUART_RXD_PIN);
+Adafruit_BluefruitLE_UART ble(bluefruitSS, BLUEFRUIT_UART_MODE_PIN, BLUEFRUIT_UART_CTS_PIN, BLUEFRUIT_UART_RTS_PIN);
 #endif
 
 const int ledPin = 13;  // led pin
@@ -25,136 +25,157 @@ const int sdChipSelectPin = 53; //For Mega TFT LCD shield use 53, for SD card re
 String messageBuffer = "";
 bool recordMessage = false;
 
+SdFat SD;
+
 void setup() {
-  while (!Serial);  // required for Flora & Micro
-  delay(500);
-  
-  Serial.begin(115200);
+	Serial.begin(115200);
+	// Wait for USB Serial
+	while (!Serial) {
+		SysCall::yield();
+	}
+	delay(1000);
 
-  Serial.print(F("Initializing SD card..."));
+	Serial.println(F("\nBleSdRemote - START\n"));
 
-  if (!SD.begin(sdChipSelectPin)) {
-    Serial.println(F(" Failed!"));
-    return;
-  }
-  Serial.println(F(" Done."));
+	Serial.print(F("Initializing SD card..."));
+	pinMode(sdChipSelectPin, OUTPUT);
+	if (!SD.begin(sdChipSelectPin, SPI_HALF_SPEED)) {
+		//Stop the sketch execution
+		SD.initErrorHalt();
+	} else {
+		Serial.println(F(" Done."));
+	}
 
-  Serial.println(F("Adafruit Bluefruit Command <-> Data Mode Example"));
-  Serial.println(F("------------------------------------------------"));
+	Serial.println(F("Adafruit Bluefruit Command <-> Data Mode Example"));
+	Serial.println(F("------------------------------------------------"));
 
-  /* Initialise the module */
-  Serial.print(F("Initialising the Bluefruit LE module: "));
+	/* Initialise the module */
+	Serial.print(F("Initialising the Bluefruit LE module: "));
 
-  if (!ble.begin(VERBOSE_MODE)) {
-    Serial.println(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
-  }
-  Serial.println(F("OK!"));
+	if (!ble.begin(VERBOSE_MODE)) {
+		Serial.println(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+	}
+	Serial.println(F("OK!"));
 
-  if (FACTORYRESET_ENABLE) {
-    /* Perform a factory reset to make sure everything is in a known state */
-    Serial.println(F("Performing a factory reset: "));
-    if ( ! ble.factoryReset() ){
-      Serial.println(F("Couldn't do factory reset"));
-    }
-  }
+	if (FACTORYRESET_ENABLE) {
+		/* Perform a factory reset to make sure everything is in a known state */
+		Serial.println(F("Performing a factory reset: "));
+		if (!ble.factoryReset()) {
+			Serial.println(F("Couldn't do factory reset"));
+		}
+	}
 
-  ble.echo(false);
-  ble.info();  
-  ble.verbose(false); 
+	ble.echo(false);
+	ble.info();
+	ble.verbose(false);
 
-  Serial.println(F("Please use Adafruit Bluefruit LE app to connect in UART mode"));
-  Serial.println(F("Then Enter characters to send to Bluefruit"));
-  Serial.println();
+	Serial.println(F("Please use Adafruit Bluefruit LE app to connect in UART mode"));
+	Serial.println(F("Then Enter characters to send to Bluefruit"));
+	Serial.println();
 
-  /* Wait for connection */
-  while (!ble.isConnected()) {
-      delay(500);
-  }
+	/* Wait for connection */
+	while (!ble.isConnected()) {
+		delay(500);
+	}
 
-  // Set module to DATA mode
-  ble.setMode(BLUEFRUIT_MODE_DATA);
-  Serial.println(F("**************************"));
-  Serial.println(F("* Switched to DATA mode! *"));
-  Serial.println(F("**************************")); 
+	// Set module to DATA mode
+	ble.setMode(BLUEFRUIT_MODE_DATA);
+	Serial.println(F("**************************"));
+	Serial.println(F("* Switched to DATA mode! *"));
+	Serial.println(F("**************************"));
 }
 
 void loop() {
 
-  //Read messages from the standard Serial interface
-  if (Serial.available() > 0) {
-    readMessageFromSerial(Serial.read());
-  }
+	//Read messages from the standard Serial interface
+	if (Serial.available() > 0) {
+		readMessageFromSerial(Serial.read());
+	}
 
-  //Read messages from the BLE interface  
-  while (ble.available()) {
-    readMessageFromSerial((char)ble.read());
-  }  
+	//Read messages from the BLE interface
+	while (ble.available()) {
+		readMessageFromSerial((char) ble.read());
+	}
 }
 
-void readMessageFromSerial(char data) {  
-  if (data == '@') {
-    recordMessage = true;
-    digitalWrite(ledPin, HIGH); //Turn the LED on
-  } else if (data == '#') {
-    handleMessage(messageBuffer);
-    recordMessage = false;
-    messageBuffer = "";    
-    digitalWrite(ledPin, LOW); //Turn the LED off      
-  } else {
-    if (recordMessage) {
-      messageBuffer += data;
-    }    
-  }  
+void readMessageFromSerial(char data) {
+	if (data == '@') {
+		recordMessage = true;
+		digitalWrite(ledPin, HIGH); //Turn the LED on
+	} else if (data == '#') {
+		handleMessage(messageBuffer);
+		recordMessage = false;
+		messageBuffer = "";
+		digitalWrite(ledPin, LOW); //Turn the LED off
+	} else {
+		if (recordMessage) {
+			messageBuffer += data;
+		}
+	}
 }
 
 void handleMessage(String message) {
-  Serial.println(message);    
-  Serial.flush();
-  
-  String responseMessage = "";      
-  if (message.startsWith("LIST:") || message.startsWith("list:")) {
-    File directory = SD.open(extractFileName(message), FILE_READ);    
-    responseMessage += listDirectory(directory);       
-  } else if (message.startsWith("LIST") || message.startsWith("list")) {  
-    File root = SD.open("/", FILE_READ);  
-    responseMessage += listDirectory(root);    
-  } else if (message.startsWith("GET:") || message.startsWith("get:") ) {
-    String fileName = extractFileName(message);
-    //TODO Send the file content trought the serial channel
-  } else {
-    responseMessage += "ERROR";
-  }  
+	Serial.println(message);
+	Serial.flush();
 
-  Serial.println(responseMessage);    
-  Serial.flush(); 
-  ble.print(responseMessage);    
-  ble.flush(); 
+	String responseMessage = "";
+	if (message.startsWith("LIST:") || message.startsWith("list:")) {
+
+		//The String has to be converted into a char array, otherwise the board will reset itself
+		String directoryPath = extractDirectoryPath(message);
+		char directoryPathArray[directoryPath.length()+1];
+		directoryPath.toCharArray(directoryPathArray, directoryPath.length()+1);
+
+		if (SD.chdir(directoryPathArray)) {
+			responseMessage += listDirectory(SD.vwd());
+			SD.chdir(); //Change back to the ROOT directory
+		} else {
+			Serial.print("Failed to open ");
+			Serial.println(directoryPathArray);
+			responseMessage += "ERROR";
+		}
+	} else if (message.startsWith("LIST") || message.startsWith("list")) {
+		//Change back to the ROOT directory
+		if (SD.chdir()) {
+			responseMessage += listDirectory(SD.vwd());
+		} else {
+			responseMessage += "ERROR";
+		}
+	} else if (message.startsWith("GET:") || message.startsWith("get:")) {
+		//TODO Send the file content through the serial channel
+	} else {
+		responseMessage += "ERROR";
+	}
+
+	Serial.println(responseMessage);
+	Serial.flush();
+	ble.print(responseMessage);
+	ble.flush();
 }
 
-String extractFileName(String message)
-{
-  String fileName = message.substring(5, message.length());
-  Serial.print(F("File name: "));
-  Serial.println(fileName);
-  return fileName;    
+String extractDirectoryPath(String message) {
+	String directoryPath = message.substring(5, message.length());
+	Serial.print(F("Directory path: "));
+	Serial.println(directoryPath);
+	delay(1000);
+	return directoryPath;
 }
 
-String listDirectory(File dir) {
-  String responseMessage = "";
-  while (true) {
-    File entry = dir.openNextFile();
-    if (!entry) {
-      //No more files - return to the first file in the directory      
-      entry.close();
-      dir.close();
-      dir.rewindDirectory();
-      return responseMessage;
-    }
-    responseMessage += entry.name();
-    if (entry.isDirectory()) {
-      responseMessage += "/";
-    } 
-    responseMessage += ",";
-    entry.close();
-  }
+String listDirectory(FatFile* currentDir) {
+	String responseMessage = "";
+	char longFileName[256];
+	while (true) {
+		SdFile currentFile;
+		if (!currentFile.openNext(currentDir, O_READ)) {
+			//No more files
+			return responseMessage;
+		}
+		currentFile.getName(longFileName, 256);
+		responseMessage += longFileName;
+		if (currentFile.isDir()) {
+			responseMessage += "/";
+		}
+		responseMessage += ",";
+		currentFile.close();
+	}
 }
